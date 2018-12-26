937dfe32e6ffb477b40687b5de35e3ed0b4ee491
Cjewett_work <Craig9907@gmail.com>
2018-12-25 17:30:49 -0800

    Update routes with tweets route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log("Connected to MongoDB "))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Another World!!"));
@@ -21,3 +21,12 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+// const mongose = require("monogoose")
+
+// const db = require('./config/keys').mongoURI;
+// mongoose.connect(db, {useNewUrlParser: true})
+    // .then() console.log("connected to mongo db")
+    // .catch(err => console.log(err)); 
--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
+
+
+
+router.get("/test", (req, res) => res.json({ msg: "This is the tweet route" }));
 
 
   module.exports = router;
